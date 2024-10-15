a5beaaa6f3a343683592765e2d0301121915339c
MK <qscvdefb@gmail.com>
2024-10-15 15:10:29 +0700

    custom interceptor

--- src/main/java/com/javaweb/Interceptor/Vip3Interceptor.java
+++ src/main/java/com/javaweb/Interceptor/Vip3Interceptor.java
@@ -3,15 +3,14 @@ package com.javaweb.Interceptor;
 import com.javaweb.model.mongo_entity.userData;
 import com.javaweb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
-public class Vip1Interceptor extends HandlerInterceptorAdapter {
+@Component
+public class Vip3Interceptor extends HandlerInterceptorAdapter {
     @Autowired
     UserRepository userRepository;
 
@@ -20,14 +19,14 @@ public class Vip1Interceptor extends HandlerInterceptorAdapter {
             HttpServletRequest request,
             HttpServletResponse response,
             Object handler
-            ) throws Exception {
+    ) throws Exception {
 
         String username = request.getAttribute("username").toString();
 
         userData user = userRepository.findByUsername(username);
 
         System.out.println(user);
-        if(user.getVip_role() < 1) {
+        if(user.getVip_role() < 3) {
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Vip role không đáp ứng yêu cầu truy cập api!");
             return false;
         }
